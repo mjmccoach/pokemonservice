ed72534bac385431efc83bc2fea6a381ee57b17f
Matthew <m.mccoach88@gmail.com>
2025-11-12 13:08:37 +0000

    Initial commit.

---
+++ src/main/java/com/projectpokemon/pokemonservice/PokemonserviceApplication.java
@@ -0,0 +1,13 @@
+package com.projectpokemon.pokemonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectpokemon/pokemonservice/PokemonserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpokemon.pokemonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
