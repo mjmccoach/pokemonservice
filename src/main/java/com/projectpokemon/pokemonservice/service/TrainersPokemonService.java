97e64526f71d9e9d0b48823e9fcfd72e9dacd31d
Matthew <m.mccoach88@gmail.com>
2025-11-17 23:48:11 +0000

    MMD-34 : MINOR : Skeleton trainers pokemondao.

--- src/main/java/com/projectpokemon/pokemonservice/service/TrainersPokemonService.java
+++ src/main/java/com/projectpokemon/pokemonservice/service/TrainersPokemonService.java
@@ -1,9 +1,14 @@
 package com.projectpokemon.pokemonservice.service;
 
+import com.projectpokemon.pokemonservice.persistence.TrainersPokemonDAO;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class TrainersPokemonService {
+
+    private TrainersPokemonDAO trainersPokemonDAO;
 
     public void getTrainersPokemonById() {
     }
