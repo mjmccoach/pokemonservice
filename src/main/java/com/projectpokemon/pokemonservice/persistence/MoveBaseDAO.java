b7ede1563b29d8f3a93c9190af9bd76f83b7d1c2
Matthew <m.mccoach88@gmail.com>
2025-12-15 17:16:42 +0000

    MMD-73 : MINOR : Add sql queries, rowmapper and base object for MoveBase.

---
+++ src/main/java/com/projectpokemon/pokemonservice/persistence/MoveBaseDAO.java
@@ -0,0 +1,32 @@
+package com.projectpokemon.pokemonservice.persistence;
+
+import com.projectpokemon.pokemonservice.objects.MoveBase;
+import com.projectpokemon.pokemonservice.persistence.rowmapper.MoveBaseRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MoveBaseDAO {
+
+    private static final String SELECT_BY_ID = "SELECT * FROM move_base WHERE id = ?";
+    private static final String SELECT_BY_TYPE = "SELECT * FROM move_base WHERE type = ?";
+    private static final String SELECT_ALL = "SELECT * FROM move_base";
+    private static final String INSERT = "INSERT INTO move_base VALUES (?, ?, ?)";
+
+    private MoveBaseRowMapper moveBaseRowMapper;
+    private JdbcTemplate jdbcTemplate;
+
+    public MoveBase getMoveBaseById(int id) {
+        return jdbcTemplate.queryForObject(SELECT_BY_ID, moveBaseRowMapper, id);
+    }
+
+    public List<MoveBase> getMoveBaseById(String type) {
+        return jdbcTemplate.query(SELECT_BY_TYPE, moveBaseRowMapper, type);
+    }
+
+    public List<MoveBase> getAllMoves() {
+        return jdbcTemplate.query(SELECT_ALL, moveBaseRowMapper);
+    }
+}
