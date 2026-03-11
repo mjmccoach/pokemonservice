d99900738cff9f42ca60e70d13718a739285005c
Matthew <m.mccoach88@gmail.com>
2026-03-11 16:57:34 +0000

    MMD-186 : MINOR : Add resistance to rock pokemon interface.

--- src/main/java/com/projectpokemon/pokemonservice/factory/rockPokemon/RockPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/rockPokemon/RockPokemonInterface.java
@@ -1,6 +1,13 @@
 package com.projectpokemon.pokemonservice.factory.rockPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface RockPokemonInterface extends Pokemon {
+    @Override
+    default List<PokemonType> getResistances() {
+        return List.of(PokemonType.NORMAL, PokemonType.FIRE, PokemonType.POISON, PokemonType.FLYING);
+    }
 }
