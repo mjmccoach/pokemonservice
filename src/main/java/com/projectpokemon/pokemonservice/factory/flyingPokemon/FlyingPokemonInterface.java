9a57eccf62bf0d9e9737b5ee90f4e06e38eae338
Matthew <m.mccoach88@gmail.com>
2026-03-12 16:28:54 +0000

    MMD-193 : MINOR : Add flying pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/flyingPokemon/FlyingPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/flyingPokemon/FlyingPokemonInterface.java
@@ -1,6 +1,12 @@
 package com.projectpokemon.pokemonservice.factory.flyingPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface FlyingPokemonInterface extends Pokemon {
+    default List<PokemonType> getFlyingResistances() {
+        return List.of(PokemonType.FIGHTING, PokemonType.BUG, PokemonType.GRASS);
+    }
 }
