d7c010ef3c08f13b33832621ab8f70b62b7faafa
Matthew <m.mccoach88@gmail.com>
2026-03-12 15:46:44 +0000

    MMD-188 : MINOR : Add poison pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/poisonPokemon/PoisonPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/poisonPokemon/PoisonPokemonInterface.java
@@ -1,6 +1,12 @@
 package com.projectpokemon.pokemonservice.factory.poisonPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface PoisonPokemonInterface extends Pokemon {
+    default List<PokemonType> getPsychicResistances() {
+        return List.of(PokemonType.GRASS, PokemonType.FIGHTING, PokemonType.POISON, PokemonType.BUG, PokemonType.FAIRY);
+    }
 }
