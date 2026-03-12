4af3e6a63a10142dc572d85399f2a6187cfa1b32
Matthew <m.mccoach88@gmail.com>
2026-03-12 15:40:45 +0000

    MMD-187 : MINOR : Add psychic pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/psychicPokemon/PsychicPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/psychicPokemon/PsychicPokemonInterface.java
@@ -1,6 +1,12 @@
 package com.projectpokemon.pokemonservice.factory.psychicPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface PsychicPokemonInterface extends Pokemon {
+    default List<PokemonType> getPsychicResistances() {
+        return List.of(PokemonType.FIGHTING, PokemonType.PSYCHIC);
+    }
 }
