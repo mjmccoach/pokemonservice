300186795ac230bac932539ea9ad4e45f8f28f80
Matthew <m.mccoach88@gmail.com>
2026-03-12 16:12:47 +0000

    MMD-191 : MINOR : Add grass pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/grassPokemon/GrassPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/grassPokemon/GrassPokemonInterface.java
@@ -1,9 +1,16 @@
 package com.projectpokemon.pokemonservice.factory.grassPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
+
+import java.util.List;
 
 public interface GrassPokemonInterface extends Pokemon {
     default boolean canEvolveWithLeafStone() {
         return false;
+    }
+
+    default List<PokemonType> getGrassResistances() {
+        return List.of(PokemonType.WATER, PokemonType.ELECTRIC, PokemonType.GRASS, PokemonType.GROUND);
     }
 }
