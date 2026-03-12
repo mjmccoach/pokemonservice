af8fc5e2d282143644630902db8e76fbbc7329e8
Matthew <m.mccoach88@gmail.com>
2026-03-12 16:20:49 +0000

    MMD-192 : MINOR : Add ghost pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/ghostPokemon/GhostPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/ghostPokemon/GhostPokemonInterface.java
@@ -1,6 +1,12 @@
 package com.projectpokemon.pokemonservice.factory.ghostPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface GhostPokemonInterface extends Pokemon {
+    default List<PokemonType> getGhostResistances() {
+        return List.of(PokemonType.POISON, PokemonType.BUG);
+    }
 }
