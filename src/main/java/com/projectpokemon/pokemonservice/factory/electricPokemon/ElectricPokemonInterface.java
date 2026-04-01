c229290c10284cb52f8e5f1202760e247d8d6905
Matthew <m.mccoach88@gmail.com>
2026-04-01 01:04:44 +0100

    MMD-196 : MINOR : Add electric pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/electricPokemon/ElectricPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/electricPokemon/ElectricPokemonInterface.java
@@ -1,10 +1,17 @@
 package com.projectpokemon.pokemonservice.factory.electricPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
+
+import java.util.List;
 
 public interface ElectricPokemonInterface extends Pokemon {
 
     default boolean evolvesWithThunderStone() {
         return false;
+    }
+
+    default List<PokemonType> getElectricResistances() {
+        return List.of(PokemonType.ELECTRIC, PokemonType.STEEL, PokemonType.FLYING);
     }
 }
