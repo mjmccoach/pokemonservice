7b8d03b7f0c91058cc080a0952e99a3bdb448519
Matthew <m.mccoach88@gmail.com>
2026-04-02 23:26:22 +0100

    MMD-197 : Add dragon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/dragonPokemon/DragonPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/dragonPokemon/DragonPokemonInterface.java
@@ -1,6 +1,12 @@
 package com.projectpokemon.pokemonservice.factory.dragonPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface DragonPokemonInterface extends Pokemon {
+    default List<PokemonType> getDragonResistances() {
+        return List.of(PokemonType.ELECTRIC, PokemonType.FIRE, PokemonType.WATER, PokemonType.GRASS);
+    }
 }
