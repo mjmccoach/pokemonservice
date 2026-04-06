0de6567f436e0d1c2147158fcab8d994b2ed6af2
Matthew <m.mccoach88@gmail.com>
2026-04-06 23:44:21 +0100

    MMD-198 : MINOR : Add bug pokemon resistances.

--- src/main/java/com/projectpokemon/pokemonservice/factory/bugPokemon/BugPokemonInterface.java
+++ src/main/java/com/projectpokemon/pokemonservice/factory/bugPokemon/BugPokemonInterface.java
@@ -1,6 +1,12 @@
 package com.projectpokemon.pokemonservice.factory.bugPokemon;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import com.projectpokemon.pokemonservice.factory.Pokemon;
 
+import java.util.List;
+
 public interface BugPokemonInterface extends Pokemon {
+    default List<PokemonType> bugPokemonResistances() {
+        return List.of(PokemonType.GRASS, PokemonType.FIGHTING, PokemonType.GROUND);
+    }
 }
