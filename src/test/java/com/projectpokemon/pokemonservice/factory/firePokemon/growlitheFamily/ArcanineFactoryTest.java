8ba591fd58c58b4084d009595f16d4a6de979633
Matthew <m.mccoach88@gmail.com>
2026-04-23 17:02:03 +0100

    MMD-203 : MINOR : Add unit tests to Arcanine Factory for fire pokemon resistances.

--- src/test/java/com/projectpokemon/pokemonservice/factory/firePokemon/growlitheFamily/ArcanineFactoryTest.java
+++ src/test/java/com/projectpokemon/pokemonservice/factory/firePokemon/growlitheFamily/ArcanineFactoryTest.java
@@ -1,5 +1,6 @@
 package com.projectpokemon.pokemonservice.factory.firePokemon.growlitheFamily;
 
+import com.projectpokemon.pokemonservice.enums.PokemonType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -34,5 +35,18 @@ class ArcanineFactoryTest {
     @Test
     void does_not_evolve_by_level() {
         assertEquals(0, arcanineFactory.getEvolutionLevel());
+    }
+
+    @Test
+    void has_resistances() {
+        assertEquals(6, arcanineFactory.getFireResistances().size());
+        assertEquals(PokemonType.FIRE, arcanineFactory.getFireResistances().getFirst());
+        assertEquals(PokemonType.GRASS, arcanineFactory.getFireResistances().get(1));
+        assertEquals(PokemonType.ICE, arcanineFactory.getFireResistances().get(2));
+        assertEquals(PokemonType.BUG, arcanineFactory.getFireResistances().get(3));
+        assertEquals(PokemonType.STEEL, arcanineFactory.getFireResistances().get(4));
+        assertEquals(PokemonType.FAIRY, arcanineFactory.getFireResistances().get(5));
+
+
     }
 }
